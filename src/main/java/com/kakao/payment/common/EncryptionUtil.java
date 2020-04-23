fc78d1f0b8c40ee00eac36a44e2932a54adf04d0
chjkw <chjkw84@gmail.com>
2020-04-24 07:50:47 +0900

    Modify: static initializer

--- src/main/java/com/kakao/payment/common/EncryptionUtil.java
+++ src/main/java/com/kakao/payment/common/EncryptionUtil.java
@@ -3,16 +3,12 @@ package com.kakao.payment.common;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 
 public class EncryptionUtil {
-    public static StandardPBEStringEncryptor encryptor = init();
+    public static StandardPBEStringEncryptor encryptor;
 
-    private static StandardPBEStringEncryptor init() {
-        if(encryptor != null) return encryptor;
-
+    static {
         encryptor = new StandardPBEStringEncryptor();
         encryptor.setPassword("somePassword");
         encryptor.setAlgorithm("PBEWithMD5AndDES");
-
-        return encryptor;
     }
 
     public static String encrypt(String s) {
